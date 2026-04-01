fe9451e08dbac38cb9d4303c4e51025e90b46965
Wallace Sanches <sancheswallace@gmail.com>
2026-04-01 18:27:46 -0300

    v3

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto 2');
+    exibirTextoNaTela('h1', 'Jogo do número secreto 3');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
